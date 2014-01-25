282e0aed3c8e29f1037dcc59bde49afc7d90e0d0
J. Almazan <jon.almazan@gmail.con>
2014-01-25 16:54:13 +0100

    new tag for features selection

--- prepare_opts.m
+++ prepare_opts.m
@@ -124,9 +124,10 @@ if opts.bagging
 else
     tagBagging = '_noBagging';
 end
+tagFeats = '_FV';
 
 opts.tagPHOC = sprintf('_PHOCs%s%s%s',tagLevels,tagLevelsB,tagNumB);
-opts.tagFeatures = sprintf('_FV%s%s%s',tagPCA,tagGMM,tagFold);
+opts.tagFeatures = sprintf('%s%s%s%s',tagFeats,tagPCA,tagGMM,tagFold);
 
 % Paths and files
 opts.pathData = './data/files';
