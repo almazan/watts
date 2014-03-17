7cace11290077a4dc4c2107bb3258fd227e178a9
almazan <jon.almazan@gmail.con>
2014-03-17 10:13:34 +0100

    readded support for GW, ESP and ICDAR11

--- load_dataset.m
+++ load_dataset.m
@@ -10,14 +10,11 @@ if ~exist(opts.fileData, 'file')
     elseif strcmpi(opts.dataset,'SVT')        
         data = load_SVT(opts);
     elseif strcmpi(opts.dataset,'ICDAR11')
-        error('Not working');
-        [data,images] = load_ICDAR11(opts);
+        data = load_ICDAR11(opts);
     elseif strcmpi(opts.dataset,'GW')
-        error('Not working');
-        [data,images] = load_GW(opts);
+        data = load_GW(opts);
     elseif strcmpi(opts.dataset,'ESP')
-        error('Not working');
-        [data,images] = load_ESP(opts);    
+        data = load_ESP(opts);    
     elseif strcmpi(opts.dataset,'LP')        
         data = load_LP(opts);
     end
